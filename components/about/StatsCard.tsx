c849790f51ea7de648c4000ff0d816f27fdba985
Luis Anaya <brothersailorwebdev@gmail.com>
2022-11-30 17:14:39 +0100

    adding env

--- components/about/StatsCard.tsx
+++ components/about/StatsCard.tsx
@@ -4,7 +4,7 @@ export const StatsCard = ({ stat }) => {
       <h3 className="text-5xl font-semibold text-gray-50 lg:text-6xl">
         {stat.value}
       </h3>
-      <p className="mt-1 text-sm font-medium text-gray-300 lg:mt-2 lg:text-base">
+      <p className="mt-1 text-sm font-medium text-gray-300 md:mt-1.5 lg:mt-2 lg:text-base">
         {stat.label}
       </p>
     </li>
