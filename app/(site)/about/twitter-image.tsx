aec8949ca9daa9f8e64df78b4e946878a20f171a
Luis Anaya <brothersailorwebdev@gmail.com>
2023-05-26 20:44:49 +0200

    adding open graph

--- app/(site)/about/twitter-image.tsx
+++ app/(site)/about/twitter-image.tsx
@@ -1,5 +1,7 @@
 import { ImageResponse } from 'next/server';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 // Route segment config
 export const runtime = 'edge';
 
@@ -15,9 +17,21 @@ export const contentType = 'image/png';
 export default function Image() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      <div className="flex h-full w-full items-center justify-center bg-white text-9xl">
-        About Bluefin Aviation
+      <div className="grid h-screen place-content-center place-items-center bg-gradient-to-br from-blue-700 to-cyan-500">
+        <div className="flex flex-col items-center gap-y-3 text-center">
+          <Avatar className="h-24 w-24 border-2 border-blue-900 bg-white sm:h-32 sm:w-32">
+            <AvatarImage
+              src="/images/bluefin-logo.png"
+              alt="Bluefin Aviation"
+              className=""
+            />
+            <AvatarFallback>BF</AvatarFallback>
+          </Avatar>
+
+          <h1 className="font-gray-50 text-lg font-bold uppercase text-gray-50 sm:text-2xl">
+            About Bluefin Aviation
+          </h1>
+        </div>
       </div>
     ),
     // ImageResponse options
