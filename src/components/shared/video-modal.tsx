ff4448a26daa50f4318df24b3fe9a081817fdd3a
Luis Anaya <brothersailorwebdev@gmail.com>
2024-02-16 19:58:14 +0100

    change poster of video

--- src/components/shared/video-modal.tsx
+++ src/components/shared/video-modal.tsx
@@ -40,7 +40,11 @@ export const VideoModal = ({
               leaveTo='opacity-0 scale-95'
             >
               <Dialog.Panel className='rounded-lg-2xl relative flex aspect-video w-full max-w-3xl items-center justify-center overflow-hidden bg-white text-left align-middle shadow transition-all'>
-                <NextVideo src={promoVideo} accentColor='#1d63ab' />
+                <NextVideo
+                  src={promoVideo}
+                  accentColor='#1d63ab'
+                  poster='/images/video-poster.webp'
+                />
                 {/* <video autoPlay loop controls className='w-[800px]'>
                   <source src={videoURL} />
                 </video> */}
