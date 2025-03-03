b34629c880b640747a46131e05768701411d64e4
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-03 11:20:56 +0100

    fix: padding left logo navbar

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -68,7 +68,7 @@ export const Navbar = () => {
   return (
     <header
       className={cn(
-        "fixed top-0 sm:p-0 p-4 left-0 right-0 z-50 transition-colors duration-300",
+        "fixed top-0 sm:p-0 sm:pl-4 p-4 left-0 right-0 z-50 transition-colors duration-300",
         isScrolled ? "bg-white shadow-sm" : "bg-transparent"
       )}
     >
