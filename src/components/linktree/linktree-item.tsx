270d5ab6cc215b0e5c1fb92aa5f5075c5f555776
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-03 07:45:48 +0100

    fix

--- src/components/linktree/linktree-item.tsx
+++ src/components/linktree/linktree-item.tsx
@@ -6,7 +6,7 @@ export const LinktreeItem = ({ link }: { link: LinktreeLink }) => {
     <li>
       <Link
         href={link.linkUrl ?? "/"}
-        className="tw-transition gap-y-2.5w group grid rounded-2xl border-2 border-blue-900 bg-white p-5 text-center text-blue-900 hover:scale-105"
+        className="tw-transition gap-y-2.5 group grid rounded-2xl border-2 border-blue-900 bg-white p-5 text-center text-blue-900 hover:scale-105"
       >
         <h2 className="text-base font-semibold sm:text-lg">{link.label}</h2>
         <p className="text-sm text-zinc-900">{link.description}</p>
