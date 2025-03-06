1fc786f993e4a7964d5fe9f5c649a28d0389d59c
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-06 15:02:43 +0100

    fix: change time of carousel

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -1,39 +1,39 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { PageSummary } from "@/components/shared/page-summary";
-import { PageTitle } from "@/components/shared/page-title";
-import { Container } from "@/components/shared/section-container";
-import { buttonVariants } from "@/components/ui/button";
+import { PageSummary } from '@/components/shared/page-summary'
+import { PageTitle } from '@/components/shared/page-title'
+import { Container } from '@/components/shared/section-container'
+import { buttonVariants } from '@/components/ui/button'
 
 type HeroImage = {
-  src: string;
-  alt: string;
-};
+  src: string
+  alt: string
+}
 
 const heroImages: HeroImage[] = [
-  { src: "/images/home-hero-1.webp", alt: "Private plane in flight." },
-  { src: "/images/home-hero-2.webp", alt: "Luxury private jet interior." },
-  { src: "/images/home-hero-3.webp", alt: "Plane in the middle of the sky." },
-];
+  { src: '/images/home-hero-1.webp', alt: 'Private plane in flight.' },
+  { src: '/images/home-hero-2.webp', alt: 'Luxury private jet interior.' },
+  { src: '/images/home-hero-3.webp', alt: 'Plane in the middle of the sky.' }
+]
 
 export const HeroSection = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % heroImages.length);
-    }, 7000); // Change image every 7 seconds
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % heroImages.length)
+    }, 5000) // Change image every 7 seconds
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <>
-      <div className="grid sm:h-screen sm:py-0 py-24 items-center justify-center overflow-hidden relative">
+      <div className='relative grid items-center justify-center overflow-hidden py-24 sm:h-screen sm:py-0'>
         {heroImages.map((image, index) => (
           <Image
             key={image.src}
@@ -43,35 +43,35 @@ export const HeroSection = () => {
             priority={index === 0}
             quality={100}
             className={`object-cover object-center transition-opacity duration-1000 ${
-              index === currentImageIndex ? "opacity-100" : "opacity-0"
+              index === currentImageIndex ? 'opacity-100' : 'opacity-0'
             }`}
           />
         ))}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
+        <div className='absolute inset-0 bg-gradient-to-t from-black/80 to-transparent' />
         <Container>
-          <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
-                                  <div className="relative z-10  mt-6 max-w-2xl    mx-auto sm:mt-0 sm:max-w-none lg:col-span-7 lg:pt-6 xl:col-span-6">
-              <PageTitle className="text-white">{`The Best Solution for Your Aviation Needs`}</PageTitle>
-              <PageSummary className="text-slate-200">
+          <div className='lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20'>
+            <div className='relative z-10 mx-auto mt-6 max-w-2xl sm:mt-0 sm:max-w-none lg:col-span-7 lg:pt-6 xl:col-span-6'>
+              <PageTitle className='text-white'>{`The Best Solution for Your Aviation Needs`}</PageTitle>
+              <PageSummary className='text-slate-200'>
                 {`A total concierge-style battery of services awaits you at Bluefin Aviation. We offer our clients top quality services for aircraft needs at all levels.`}
               </PageSummary>
-              <div className="mt-8 flex flex-wrap gap-x-3 gap-y-2 lg:gap-x-6 lg:gap-y-4">
+              <div className='mt-8 flex flex-wrap gap-x-3 gap-y-2 lg:gap-x-6 lg:gap-y-4'>
                 <Link
-                  href="/services"
+                  href='/services'
                   className={buttonVariants({
-                    variant: "default",
-                    size: "lg",
+                    variant: 'default',
+                    size: 'lg'
                   })}
                 >
                   Our Services
                 </Link>
                 <Link
-                  href="https://wa.me/+19548812932"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://wa.me/+19548812932'
+                  target='_blank'
+                  rel='noopener noreferrer'
                   className={buttonVariants({
-                    variant: "secondary",
-                    size: "lg",
+                    variant: 'secondary',
+                    size: 'lg'
                   })}
                 >
                   24/7 Support
@@ -83,5 +83,5 @@ export const HeroSection = () => {
         </Container>
       </div>
     </>
-  );
-};
+  )
+}
--- src/components/home/testimonials-carousel.tsx
+++ src/components/home/testimonials-carousel.tsx
@@ -1,41 +1,41 @@
-"use client";
+'use client'
 
-import Autoplay from "embla-carousel-autoplay";
+import Autoplay from 'embla-carousel-autoplay'
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import { Testimonial } from "@/sanity/types";
+  CarouselItem
+} from '@/components/ui/carousel'
+import { Testimonial } from '@/sanity/types'
 
 export const TestimonialsCarousel = ({
-  testimonials,
+  testimonials
 }: {
-  testimonials: Testimonial[];
+  testimonials: Testimonial[]
 }) => {
   return (
     <Carousel
       opts={{
-        align: "start",
-        loop: true,
+        align: 'start',
+        loop: true
       }}
       plugins={[
         Autoplay({
-          delay: 5000,
-        }),
+          delay: 7000
+        })
       ]}
-      className="w-full"
+      className='w-full'
     >
       <CarouselContent>
         {testimonials.map((testimonial, idx) => (
           <CarouselItem
             key={idx}
-            className="mt-8 flex cursor-pointer flex-col justify-center lg:mt-0"
+            className='mt-8 flex cursor-pointer flex-col justify-center lg:mt-0'
           >
-            <p className="text-base italic sm:text-xl text-primary font-medium">{`${testimonial.quote}`}</p>
+            <p className='text-base font-medium text-primary italic sm:text-xl'>{`${testimonial.quote}`}</p>
             {testimonial.author && (
-              <p className="mt-4 flex items-center flex-wrap text-slate-600 gap-x-2 font-mono text-xs sm:text-sm uppercase">
-                <span className="font-medium">{testimonial.author.name}</span>
+              <p className='mt-4 flex flex-wrap items-center gap-x-2 font-mono text-xs text-slate-600 uppercase sm:text-sm'>
+                <span className='font-medium'>{testimonial.author.name}</span>
                 <span> | </span>
                 <span>{testimonial.author.role}</span>
                 <span> | </span>
@@ -46,5 +46,5 @@ export const TestimonialsCarousel = ({
         ))}
       </CarouselContent>
     </Carousel>
-  );
-};
+  )
+}
