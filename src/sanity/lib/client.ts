c652e196635addaaff8523a429bff869e3f126cf
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-13 16:33:43 +0100

    fix: queries and fetches

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,12 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from "../env";
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
+  perspective: 'published',
   useCdn: true,
-  stega: { studioUrl: "/studio" },
-});
+  stega: { studioUrl: '/studio' }
+})
